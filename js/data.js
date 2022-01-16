2eb14c3f6a3fd7ed6837702e0cae3250620c7251
cqrity <cqrity@list.ru>
2022-01-16 10:39:14 +0300

    data.js

---
+++ js/data.js
@@ -0,0 +1,43 @@
+import { getRandomPositiveInteger, getRandomPositiveFloat, createIdGenerator } from './utils.js';
+
+const OFFERS_QUANTITY = 10;
+const OFFER_TYPES = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
+const CHECK_TIMES = ['12:00', '13:00', '14:00'];
+const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+const PHOTO_LINKS = [
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
+];
+
+const generatePhotoId = createIdGenerator();
+
+function createOffer() {
+  const offerObj = {
+    author: {
+      avatar: `img/avatars/user${generatePhotoId()}.png`,
+    },
+    location: {
+      lat: getRandomPositiveFloat(35.65000, 35.70000),
+      lng: getRandomPositiveFloat(139.70000, 139.80000),
+    },
+    offer: {
+      title: 'Заголовок предложения.',
+      address: '',
+      price: getRandomPositiveInteger(6000, 60000),
+      type: OFFER_TYPES[getRandomPositiveInteger(0, OFFER_TYPES.length + 1)],
+      rooms: getRandomPositiveInteger(1, 3),
+      guests: getRandomPositiveInteger(0, 2),
+      checkin: CHECK_TIMES[getRandomPositiveInteger(0, CHECK_TIMES.length - 1)],
+      checkout: CHECK_TIMES[getRandomPositiveInteger(0, CHECK_TIMES.length - 1)],
+      features: FEATURES.slice(0, getRandomPositiveInteger(1, 6)),
+      description: 'Описание помещения.',
+      photos: PHOTO_LINKS.slice(0, getRandomPositiveInteger(1, 3)),
+    },
+  };
+  offerObj.offer.address = `${offerObj.location.lat}, ${offerObj.location.lng}`;
+  return offerObj;
+}
+
+const offers = Array.from({ length: OFFERS_QUANTITY }, createOffer);
+console.log(offers);
---
+++ js/utils.js
@@ -0,0 +1,43 @@
+const getRandomPositiveInteger = (from, to) => {
+  const lower = Math.ceil(Math.min(Math.abs(from), Math.abs(to)));
+  const upper = Math.floor(Math.max(Math.abs(from), Math.abs(to)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+const getRandomPositiveFloat = (from, to, digits = 1) => {
+  const lower = Math.min(Math.abs(from), Math.abs(to));
+  const upper = Math.max(Math.abs(from), Math.abs(to));
+  const result = Math.random() * (upper - lower) + lower;
+  return Number(result.toFixed(digits));
+};
+
+const createRandomIdFromRangeGenerator = (min, max) => {
+  const previousValues = [];
+
+  return function () {
+    let currentValue = getRandomPositiveInteger(min, max);
+    if (previousValues.length >= (max - min + 1)) {
+      return null;
+    }
+    while (previousValues.includes(currentValue)) {
+      currentValue = getRandomPositiveInteger(min, max);
+    }
+    previousValues.push(currentValue);
+    return currentValue;
+  };
+};
+
+function createIdGenerator() {
+  let lastGeneratedId = 0;
+
+  return function () {
+    lastGeneratedId += 1;
+    if (lastGeneratedId === 10) {
+      return lastGeneratedId;
+    }
+    return `0${lastGeneratedId}`;
+  };
+}
+
+export { getRandomPositiveInteger, getRandomPositiveFloat, createRandomIdFromRangeGenerator, createIdGenerator };
